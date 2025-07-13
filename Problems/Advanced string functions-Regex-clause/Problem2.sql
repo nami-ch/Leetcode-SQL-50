109d0b261d99eaa34f9b727be699d71351a79356
Namisha <96141758+nami-ch@users.noreply.github.com>
2025-07-13 22:33:46 +0530

    Update Problem2.sql

--- Problems/Advanced string functions-Regex-clause/Problem2.sql
+++ Problems/Advanced string functions-Regex-clause/Problem2.sql
@@ -1,4 +1,14 @@
+-- Solution 1: --
 
-select patient_id, patient_name, conditions
-from patients 
-where conditions like '%DIAB1%'
+SELECT patient_id, patient_name, conditions
+FROM Patients
+WHERE REGEXP_LIKE(conditions, '(^| )DIAB1\\w*( |$)');
+
+-- Solution 2: --
+
+SELECT patient_id, patient_name, conditions
+FROM Patients
+WHERE conditions LIKE 'DIAB1%'        -- begins with DIAB1
+   OR conditions LIKE '% DIAB1%'      -- in the middle
+   OR conditions LIKE '% DIAB1';      -- at the end
+
